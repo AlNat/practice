5afb56192cb822379b1658075500d1477e5a1c7f
AlNat <aleksnatarov@gmail.com>
2020-02-03 00:05:17 +0300

    Начало работы с BST

---
+++ src/main/java/dev/alnat/practice/bst/BST.java
@@ -0,0 +1,25 @@
+package dev.alnat.practice.bst;
+
+/**
+ * Binary Search Tree
+ * Двоичное дерево поиска
+ * @link https://ru.wikipedia.org/wiki/%D0%94%D0%B2%D0%BE%D0%B8%D1%87%D0%BD%D0%BE%D0%B5_%D0%B4%D0%B5%D1%80%D0%B5%D0%B2%D0%BE_%D0%BF%D0%BE%D0%B8%D1%81%D0%BA%D0%B0
+ *
+ * Created by @author AlNat on 02.02.2020.
+ * Licensed by Apache License, Version 2.0
+ */
+public class BST {
+
+    // TODO search
+
+    // TODO add
+
+    // TODO delete
+
+    // TODO merge
+
+    // TODO sum from to
+
+    // TODO
+
+}
---
+++ src/test/java/bst/BSTTest.java
@@ -0,0 +1,35 @@
+package bst;
+
+import dev.alnat.practice.bst.BST;
+import org.junit.Test;
+
+/**
+ * Created by @author AlNat on 02.02.2020.
+ * Licensed by Apache License, Version 2.0
+ */
+public class BSTTest {
+
+    private BST generateTestBST() {
+        BST bst = new BST();
+
+        // TODO
+
+        return bst;
+    }
+
+    @Test
+    public void searchTest() {
+
+    }
+
+    @Test
+    public void insertNodeTest() {
+
+    }
+
+    @Test
+    public void mergerTest() {
+
+    }
+
+}
